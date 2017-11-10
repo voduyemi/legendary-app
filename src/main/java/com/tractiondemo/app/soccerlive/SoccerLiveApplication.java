63d527084c868accb20417ecb5294846e9c6e85b
Victor Oduyemi <dav.oduyemi@gmail.com>
2017-11-09 22:02:43 -0800

    Initial commit

---
+++ src/main/java/com/tractiondemo/app/soccerlive/SoccerLiveApplication.java
@@ -0,0 +1,12 @@
+package com.tractiondemo.app.soccerlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerLiveApplication.class, args);
+	}
+}
